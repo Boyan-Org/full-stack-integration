6c9cce8261783959827f618383e2d4d82ccbb480
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-11-28 01:44:16 +0800

    Update RegForm.vue

--- src/components/RegForm.vue
+++ src/components/RegForm.vue
@@ -14,15 +14,19 @@
         </el-form-item>
         <el-form-item :error="confirmError" required="">
           <span slot="label">Confirm Password</span>
-          <el-input placeholder="Repeat Password" v-model="confirmPassword" show-password></el-input>
+          <el-input
+            placeholder="Repeat Password"
+            v-model="confirmPassword"
+            show-password
+          ></el-input>
         </el-form-item>
         <el-form-item label="Your Name" required="" :error="nameError">
-          <el-input v-model="form.name"
-            placeholder="John Smith"></el-input>
+          <el-input v-model="form.name" placeholder="John Smith"></el-input>
         </el-form-item>
         <el-button type="primary" @click="onSubmit">Register</el-button>
         <div>
-          <br />Already have an account? <el-link  type="primary" @click="open">Login</el-link>
+          <br />Already have an account?
+          <el-link type="primary" @click="open">Login</el-link>
         </div>
       </el-form>
     </div>
@@ -36,48 +40,52 @@
 </style>
 
 <script>
+import axios from "axios";
 
 export default {
   data() {
     return {
       confirmError: "",
       ruleError: "",
-      userError:"",
-      confirmPassword:"",
-      nameError:"",
+      userError: "",
+      confirmPassword: "",
+      nameError: "",
       form: {
         username: "",
         password: "",
-        name:""
+        name: "",
       },
       show: true,
     };
   },
   methods: {
-    open() { //redirect to /login
-      this.$router.push('/login');
+    open() {
+      //redirect to /login
+      this.$router.push("/login");
     },
     onSubmit(evt) {
       // TODO: remove variable display;
       console.warn("remove the display of form content");
-      console.log("username: "+this.form.username);
-      console.log("password: "+this.form.password);
-      console.log("confirm: "+ this.confirmPassword);
-      console.log("name: "+this.form.name);
-      if (this.form.username == ""){
+      console.log("username: " + this.form.username);
+      console.log("password: " + this.form.password);
+      console.log("confirm: " + this.confirmPassword);
+      console.log("name: " + this.form.name);
+      if (this.form.username == "") {
         this.userError = "Please input a valid ID";
         return;
-      } else { //TODO: username already exist
+      } else {
+        //TODO: username already exist
         this.userError = "";
       }
-      if (this.form.password == "") { //TODO: add complexity check
+      if (this.form.password == "") {
+        //TODO: add complexity check
         // Test if the password is valid
         this.ruleError = "Please input a password";
         return;
       } else {
         this.ruleError = "";
       }
-      if (this.confirmPassword == ""){
+      if (this.confirmPassword == "") {
         this.confirmError = "Please confirm your password";
         return;
       } else if (this.confirmPassword != this.form.password) {
@@ -87,12 +95,42 @@ export default {
       } else {
         this.confirmError = "";
       }
-      if (this.form.name == ""){
+      if (this.form.name == "") {
         this.nameError = "Please enter your name";
         return;
       }
       evt.preventDefault();
-      alert(JSON.stringify(this.form));
+      // alert(JSON.stringify(this.form));
+
+      axios
+        .post("api/register/", {
+          username: this.form.username,
+          password: this.form.password,
+          role: "patient",
+          name: this.form.name,
+        })
+        .then((resp) => {
+          console.log(resp);
+          this.$alert(
+            "Welcome to use EHR service. You will be redirected to the login page.",
+            "Congratulations",
+            {
+              confirmButtonText: "OK",
+              callback: () => {
+                this.$router.push("/login");
+              },
+            }
+          );
+        })
+        .catch((error) => {
+          //error handling
+          console.log(error.response.status);
+          var loginCode = error.response.status;
+          // User exist
+          if (loginCode == 409) {
+            this.userError = "Username already exists";
+          }
+        });
     },
     onReset(evt) {
       evt.preventDefault();
