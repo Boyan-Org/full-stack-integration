77b8b9c468a0f18322dfe507870659a4f9d50bc4
Frankie <zz1763@nyu.edu>
2020-12-08 14:56:44 +0800

    some_tests
    Now they are not working well

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from backend.api.models import Account
+from django.http import response
+from django.test import TestCase, Client
 
-# Create your tests here.
+# class RegisterTestCase(TestCase):
+#     def setup(self):
+#         self.client = Client()
+
+#     def test_register(self):
+#         response = self.client.post(
+#             '/api/register/',
+#             data = {
+#                     "username": "userfrank",
+#                     "password": "frank",
+#                     "role": "patient",
+#                     "name": "Frank Zhou"
+#                     },
+#         )
+#         print(response.content)
+#         self.assertEqual(response.status_code, 201)
+
+
+class LoginTestCase(TestCase):
+    def setup(self):
+        self.client = Client()
+        Account.objects.create(
+            username = "userfrank",
+            password = "frank",
+            role = "patient",
+        )
+    def test_login(self):
+        response = self.client.post(
+            '/api/login/',
+            {
+                "username": "userfrank",
+                "password": "frank",
+            }
+        )
+        print(response.content) 
+        self.assertEqual(response.status_code, 200)
+
+
+
