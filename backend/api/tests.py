c2810d684972e982ebb715ee0d7d61536fe14c3e
Frankie <zz1763@nyu.edu>
2020-12-09 15:53:47 +0800

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,32 +1,64 @@
 from backend.api.models import Account
 from django.http import response
-from django.test import TestCase, Client
+from django.test import TestCase
+from rest_framework.test import APIClient
 
-# class RegisterTestCase(TestCase):
-#     def setUp(self):
-#         self.client = Client()
+class RegisterTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
 
-#     def test_register(self):
-#         response = self.client.post(
-#             '/api/register/',
-#             data = {
-#                     "username": "userfrank",
-#                     "password": "frank",
-#                     "role": "patient",
-#                     "name": "Frank Zhou"
-#                     },
-#         )
-#         print(response.content)
-#         self.assertEqual(response.status_code, 201)
+    def test_register(self):
+        response = self.client.post(
+            '/api/register/',
+            {
+                "username": "userfrank",
+                "password": "frank",
+                "role": "patient",
+                "name": "Frank Zhou"
+            },
+            format = 'json',
+        )
+        # print("response_content:", response.content)
+        self.assertEqual(response.status_code, 201)
+
+    def test_username_conflict(self):
+        self.client.post(
+            '/api/register/',
+            {
+                "username": "userfrank",
+                "password": "frank",
+                "role": "patient",
+                "name": "Frank Zhou"
+            },
+            format = 'json',
+        )
+        response = self.client.post(
+            '/api/register/',
+            {
+                "username": "userfrank",
+                "password": "f",
+                "role": "patient",
+                "name": "F"
+            },
+            format = 'json',
+        )
+        self.assertEqual(response.status_code, 409)
+
 
 
 class LoginTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
-        Account.objects.create(
-            username = "userfrank",
-            password = "frank",
-            role = "patient",
+        self.client = APIClient()
+        # create account and personal_info
+        self.client.post(
+            '/api/register/',
+            {
+                "username": "userfrank",
+                "password": "frank",
+                "role": "patient",
+                "name": "Frank Zhou"
+            },
+            format = 'json',
         )
     def test_login(self):
         response = self.client.post(
@@ -34,10 +66,40 @@ class LoginTestCase(TestCase):
             {
                 "username": "userfrank",
                 "password": "frank",
-            }
+            },
+            format = 'json',
         )
-        print(response.content) 
         self.assertEqual(response.status_code, 200)
 
+    def test_wrong_username(self):
+        response = self.client.post(
+            '/api/login/',
+            {
+                "username": "userfrank",
+                "password": "frank",
+            },
+            format = 'json',
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+# class PersonalInfoTestCase(TestCase):
+#     def setUp(self):
+#         self.client = APIClient()
+#         Account.objects.create(
+#             username = "userfrank",
+#             password = "frank",
+#             role = "patient",
+#         )
+#     def test_login(self):
+#         response = self.client.post(
+#             '/api/login/',
+#             {
+#                 "username": "userfrank",
+#                 "password": "frank",
+#             }
+#         )
+#         print(response.content) 
+#         self.assertEqual(response.status_code, 200)
 
 
